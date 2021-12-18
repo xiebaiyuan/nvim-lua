ec3206ee56db4788d9f20b19c0c2fb3c8ffba76b
Eatsolx <xrz@eatsol.net>
2021-12-18 18:19:56 +0800

    更新上传脚本

--- script/Upload.sh
+++ script/Upload.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 cd ..
-rm -rf ~/Desktop/nvim-lua/lua ~/Desktop/nvim-lua/plugin ~/Desktop/nvim-lua/init.lua
+rm -rf ~/Desktop/nvim-lua/lua ~/Desktop/nvim-lua/init.lua ~/Desktop/nvim-lua/README.md
 cp init.lua ~/Desktop/nvim-lua/
+cp README.md ~/Desktop/nvim-lua/
 cp -r lua ~/Desktop/nvim-lua/
+rm -f ~/Desktop/nvim-lua/script/install.sh
+rm -f ~/Desktop/nvim-lua/script/Upload.sh
+cp script/Upload.sh ~/Desktop/nvim-lua/script/
+cp script/install.sh ~/Desktop/nvim-lua/script/
 cd ~/Desktop/nvim-lua/
 echo "复制成功"
 read -s -n1 -p "按任意键继续 ... "
