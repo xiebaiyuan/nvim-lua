2d3f3f292fe2b74e28e378361013fdc13fe1bb9f
Eatsolx <xrz@eatsol.net>
2021-12-18 18:10:37 +0800

    更新安装脚本

--- script/install.sh
+++ script/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 cd ..
-mv .git git
-mv .github github
-cp -r script/NvChad/git .git
-cp -r script/NvChad/github .github
+mv ./.git git
+mv ./.github github
+cp -r script/NvChad/git ./.git
+cp -r script/NvChad/github ./.github
 mv ~/.config/nvim ~/.config/NVIM.BAK
 cp -r ../nvim-lua ~/.config/nvim
-rm -rf .git .github
-mv git .git
-mv github .github
+rm -rf ./.git ./.github
+mv git ./.git
+mv github ./.github
 rm -rf ~/.config/nvim/script
 echo "安装成功，旧配置已保存至~/.config/NVIM.BAK"
 read -s -n1 -p "按任意键继续 ... "
