940b3e5a2f006114534212c22d0e14c23e637d66
Eatsolx <xrz@eatsol.net>
2021-12-18 18:14:06 +0800

    更新安装脚本

--- script/install.sh
+++ script/install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 cd ..
-mv ./.git git
-mv ./.github github
-cp -r script/NvChad/git ./.git
-cp -r script/NvChad/github ./.github
+mv .git git
+mv .github github
+cp -r script/NvChad/git .git
+cp -r script/NvChad/github .github
 mv ~/.config/nvim ~/.config/NVIM.BAK
 cp -r ../nvim-lua ~/.config/nvim
-rm -rf ./.git ./.github
-mv git ./.git
-mv github ./.github
-rm -rf ~/.config/nvim/script
+rm -rf .git .github
+mv git .git
+mv github .github
+rm -rf ~/.config/nvim/script/NvChad
+rm -rf ~/.config/nvim/git
+rm -rf ~/.config/nvim/github
 echo "安装成功，旧配置已保存至~/.config/NVIM.BAK"
 read -s -n1 -p "按任意键继续 ... "
 nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
