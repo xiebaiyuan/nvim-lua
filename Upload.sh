8dddba5122c55c0637be41bb02114973efd5bbea
Eatsolx <xrz@eatsol.net>
2021-12-19 10:35:27 +0800

    更新安装脚本

---
+++ Upload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf ~/Desktop/nvim-lua/README.md
+cp README.md ~/Desktop/nvim-lua/
+rm -f ~/Desktop/nvim-lua/script/install.sh ~/Desktop/nvim-lua/script/Upload.sh
+cp Upload.sh ~/Desktop/nvim-lua/script/
+cp install.sh ~/Desktop/nvim-lua/script/
+rm -rf ~/Desktop/nvim-lua/custom
+cp -r lua/custom ~/Desktop/nvim-lua/
+cd ~/Desktop/nvim-lua/
+rm -rf NvChad
+git clone https://github.com/NvChad/NvChad
+rm -rf NvChad/examples
+git remote set-url origin git@github.com:Eatsolx/nvim-lua.git
+echo "复制成功"
+read -s -n1 -p "按任意键继续 ... "
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mv ~/.config/nvim ~/.config/NVIM.BAK
+cp -r NvChad ~/.config/nvim
+cp -r custom ~/.config/nvim/NvChad/
+cp install.sh ~/.config/nvim/NvChad/
+cp Upload.sh ~/.config/nvim/NvChad/
+echo "安装成功，旧配置已保存至~/.config/NVIM.BAK"
+read -s -n1 -p "按任意键继续 ... "
+nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
