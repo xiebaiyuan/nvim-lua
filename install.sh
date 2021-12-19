19abc3634a7787c7d492a54251790134661a7038
Eatsolx <xrz@eatsol.net>
2021-12-19 10:56:57 +0800

    更新install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ cp -r NvChad ~/.config/nvim
 mv ~/.config/nvim/git ~/.config/nvim/.git
 mv ~/.config/nvim/github ~/.config/nvim/.github
 cp -r custom ~/.config/nvim/lua
-cp install.sh ~/.config/nvim/NvChad/
-cp Upload.sh ~/.config/nvim/NvChad/
+cp install.sh ~/.config/nvim/
+cp Upload.sh ~/.config/nvim/
 echo "安装成功，旧配置已保存至~/.config/NVIM.BAK"
 read -s -n1 -p "按任意键继续 ... "
 nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
