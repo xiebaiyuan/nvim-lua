0507073fb7854031b8b0ba02112af6b5dd51490f
Eatsolx <xrz@eatsol.net>
2021-12-19 10:47:21 +0800

    更新install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mv ~/.config/nvim ~/.config/NVIM.BAK
-cp -r NvChad/git NvChad/.git
-cp -r NvChad/github NvChad/.github
+mv NvChad/git NvChad/.git
+mv NvChad/github NvChad/.github
 cp -r NvChad ~/.config/nvim
-cp -r custom ~/.config/nvim/NvChad/
+cp -r custom ~/.config/nvim/lua
 cp install.sh ~/.config/nvim/NvChad/
 cp Upload.sh ~/.config/nvim/NvChad/
 echo "安装成功，旧配置已保存至~/.config/NVIM.BAK"
