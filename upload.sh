38442d7ded6380142ad8580cb3668e270091d391
Eatsolx <xrz@eatsol.net>
2021-12-19 11:05:58 +0800

    测试

--- upload.sh
+++ upload.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-rm -rf ~/Desktop/nvim-lua/README.md
-cp README.md ~/Desktop/nvim-lua/
-rm -f ~/Desktop/nvim-lua/script/install.sh ~/Desktop/nvim-lua/script/Upload.sh
-cp Upload.sh ~/Desktop/nvim-lua/script/
-cp install.sh ~/Desktop/nvim-lua/script/
 rm -rf ~/Desktop/nvim-lua/custom
 cp -r lua/custom ~/Desktop/nvim-lua/
 cd ~/Desktop/nvim-lua/
 rm -rf NvChad
 git clone https://github.com/NvChad/NvChad
 rm -rf NvChad/examples
-mv NvChad/.git git
-mv NvChad/.github github
+mv NvChad/.git NvChad/git
+mv NvChad/.github NvChad/github
 git remote set-url origin git@github.com:Eatsolx/nvim-lua.git
 echo "复制成功"
 read -s -n1 -p "按任意键继续 ... "
